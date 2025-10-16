fb9a1b3ab12bf713d4761bb27d47eecd19ff9190
Orlando Alexander <67097862+orlandoalexander@users.noreply.github.com>
2025-10-16 02:10:59 +0100

    Update ProjectsSection.tsx

--- app/components/ProjectsSection.tsx
+++ app/components/ProjectsSection.tsx
@@ -15,7 +15,7 @@ const projects = [
     image2: "/visense-cover2.png",
     logo: "/visense-logo.png",
     position: "Junior Software & ML Engineer",
-    tags: ["React", "Docker", "Material-UI", "AWS", "TensorFlow", "OpenCV"],
+    tags: ["React", "Axios", "Docker", "Material-UI", "AWS", "TensorFlow", "OpenCV"],
     category: ["Software Engineering"],
   },
   {
